72216ef2cd20e8ec07cbd5ee9a1b828f1493d43d
Ummulwarahanagi <154959593+Ummulwarahanagi@users.noreply.github.com>
2025-04-04 14:44:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function reverseString(str){
+    return str.split("").reverse().join("");
+}
+console.log(reverseString("hello"));
+console.log(reverseString("ultimez"))
+console.log(reverseString("Javascript"))
+console.log(reverseString("Function"))
